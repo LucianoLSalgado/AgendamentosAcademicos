0a3643997c09837cc7e04950473c2f4c47d7d315
Luciano Lopes Salgado <lucianosalgado_83@outlook.com>
2026-06-04 16:45:19 -0300

    Ajuste metro.config.js para build

--- metro.config.js
+++ metro.config.js
@@ -6,8 +6,8 @@ const config = getDefaultConfig(__dirname);
 // Workaround para bug do RN 0.85 no iOS:
 // VirtualViewNativeComponent.js tem evento onModeChange sem args tipados
 // causando crash no codegen do Flow durante o bundling iOS
-config.resolver.blockList = [
+/*config.resolver.blockList = [
     /node_modules\/react-native\/src\/private\/components\/virtualview\/VirtualViewNativeComponent\.js$/,
 ];
 
-module.exports = config;
+module.exports = config;*/
